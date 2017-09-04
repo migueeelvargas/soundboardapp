686bf5b866cb89558a803071e17e7100de6fbde1
Hamza Khan <h.akhan@outlook.com>
2017-09-03 18:13:52 -0700

    added support for logging user in DB

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,168 +1,245 @@
 // app/routes/routes.js
-module.exports = function (app, express, conn, upload) {
+module.exports = function(app, express, conn, upload) {
+  // HOME PAGE (with login links)
+  app.get("/", function(req, res) {
+    res.render("index.ejs"); // load the index.ejs file
+  });
 
-	// HOME PAGE (with login links)
-	app.get('/', function (req, res) {
-		res.render('index.ejs')	// load the index.ejs file
-	})
+  // SIGNUP page
+  app.get("/signup", function(req, res) {
+    res.render("signup.ejs"); // load the signup.ejs file
+  });
 
-	// SIGNUP page
-	app.get('/signup', function (req, res) {		
-		res.render('signup.ejs')	// load the signup.ejs file
-	})
+ 
 
-	// CREATE NEW USER
-	app.post('/signup', function (req, res) {
-		var firstName = req.body.firstName;
-		var lastName = req.body.lastName;
-		var email = req.body.email;
-		var password = req.body.password;
+  app.post("/signup", function(req, res) {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email; //validate this...for now..
+    var password = req.body.password;
 
-		// Log user inputs
-		console.log("First Name: " + firstName);
-		console.log("Last Name: " + lastName);
-		console.log("Email: " + email);
-		console.log("Password: " + password);
+    // Log user inputs
+    console.log("First Name: " + firstName);
+    console.log("Last Name: " + lastName);
+    console.log("Email: " + email);
+    console.log("Password: " + password);
 
-		var addNewUserSQL = 
-			"INSERT INTO users (firstName, lastName, email, password) " +
-			"VALUES (\'" + firstName + "\', \'" + lastName + "\', \'" + 
-			email + "\', \'" + password + "\')";
+    var addNewUserSQL =
+      "INSERT INTO users (firstName, lastName, email, password) " +
+      "VALUES ('" +
+      firstName +
+      "', '" +
+      lastName +
+      "', '" +
+      email +
+      "', '" +
+      password +
+      "')";
 
-		// Log query string
-		console.log(addNewUserSQL);
+    //Check if email already exists...Server-Side
+    const checkEmail = 'SELECT * FROM users WHERE email = "' + email + '" ';
+    conn.query(checkEmail, function(err, result) {
+      if (err) throw err;
+      // If the result from the query is empty (bad email or password)
+      if (result.length > 0) {
+        console.log("Email already used.");
+        res.render("signup.ejs");
+      } else {
+        // Log query string
+        console.log(addNewUserSQL);
+        // Execute query command
+        conn.query(addNewUserSQL, function(err, result) {
+          if (err) throw err;
+          console.log("New user added.");
+          console.log("Result return: " + result);
+        });
 
-		var newUserID;
+        //HACK
+        var uID;
+        const getID = 'SELECT userid FROM users WHERE email = "' + email + '" ';
+        conn.query(getID, function(err, result) {
+          if (err) throw err;
+          uID = result[0].userid;
 
-		// Execute query command
-		conn.query(addNewUserSQL, function(err, result) {
-			if (err) throw err;
-			console.log("New user added.");
-			console.log("Result return: " + result);
-		})
+          const inputLog =
+            "INSERT into Logs (userid, attempts, fails, success, logouts) " +
+            "VALUES ('" +
+            parseInt(uID) +
+            "', '" +
+            "0" +
+            "', '" +
+            "0" +
+            "', '" +
+            "0" +
+            "', '" +
+            "0" +
+            "')";
+          conn.query(inputLog, function(err, result) {
+            if (err) throw err;
+            console.log("Added Log Data");
+          });
+        });
 
-		res.render('index.ejs');
-	})
+        res.redirect("/");
+      }
+    });
+  });
 
-	// LOG USER IN
-	app.post('/login', function (req, res) {
-		var email = req.body.email;
-		var password = req.body.password;
 
-		// Log user inputs
-		console.log("Email: " + email);
-		console.log("Password: " + password);
 
-		var logUserSQL = 
-			"SELECT * FROM users WHERE email = \"" + email + "\" "
-			+ "&& password = \"" + password + "\"";
+  // LOG USER IN
+  app.post("/login", function(req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
 
-		// Log query string
-		console.log(logUserSQL);
+    // Log user inputs
+    console.log("Email: " + email);
+    console.log("Password: " + password);
 
-		// Execute query call
-		conn.query(logUserSQL, function(err, result) {
-			if (err) throw err;
-			// If the result from the query is empty (bad email or password)
-			if (!result.length) {
-				console.log("User not found or incorrect password.");
-				res.redirect('/');
-			}
-			// User & password are correct & exist
-			else {
-				console.log("User found in database.");
-				req.session.email = email;
-				req.session.firstName = result[0].firstName;
-				req.session.lastName = result[0].lastName;
-				req.session.userid = result[0].userid;
+    var logUserSQL =
+      'SELECT * FROM users WHERE email = "' +
+      email +
+      '" ' +
+      '&& password = "' +
+      password +
+      '"';
 
-				console.log("Email saved in session: " + req.session.email);
-				console.log("First Name saved in session: " + req.session.firstName);
-				console.log("Last Name saved in session: " + req.session.lastName);
-				console.log("UserID saved in session: " + req.session.userid);
+    // Execute query call
+    conn.query(logUserSQL, function(err, result) {
+      if (err) throw err;
+      // If the result from the query is empty (bad email or password)
+      if (!result.length) {
+        console.log("User not found or incorrect password.");
 
-				res.redirect('/app');
-			}
-		})
-	})
+        //Get userid | attempts | fails | success | logouts
+        const getLogs =
+          "SELECT userid FROM users WHERE email=" + "'" + email + "'";
+        conn.query(getLogs, function(err, result) {
+          if (err) throw err;
+          const uID = result[0].userid;
+          const update =
+            "UPDATE Logs SET attempts = attempts + 1, fails = fails + 1 WHERE userid = " +
+            "'" +
+            uID +
+            "'";
+          conn.query(update, function(err, result) {
+            if (err) throw err;
+            console.log("attempts updated.");
+          });
+        });
 
-	// DASHBOARD AREA
-	var data = {};
+        res.render("index.ejs");
+      } else {
+        // User & password are correct & exist
+        console.log("User found in database.");
+        req.session.email = email;
+        req.session.firstName = result[0].firstName;
+        req.session.lastName = result[0].lastName;
+        req.session.userid = result[0].userid;
 
-	app.get('/app', function (req, res) {
+        console.log("Email saved in session: " + req.session.email);
+        console.log("First Name saved in session: " + req.session.firstName);
+        console.log("Last Name saved in session: " + req.session.lastName);
+        console.log("UserID saved in session: " + req.session.userid);
+        const update =
+          "UPDATE Logs SET attempts = attempts + 1, success = success + 1 WHERE userid = " +
+          "'" +
+          req.session.userid +
+          "'";
+        conn.query(update, function(err, result) {
+          if (err) throw err;
+          console.log("attempts updated.");
+        });
 
-		var getSBdataSQL = "SELECT * FROM soundboards " +
-			"WHERE userid = " + req.session.userid + " OR public = 1;";
+        res.redirect("/app");
+      }
+    });
+  });
 
-		console.log(getSBdataSQL);
+  // DASHBOARD AREA
+  var data = {};
 
-		conn.query(getSBdataSQL, function (err, result) {
-			if (err) throw err;
+  app.get("/app", function(req, res) {
+    var getSBdataSQL =
+      "SELECT * FROM soundboards " +
+      "WHERE userid = " +
+      req.session.userid +
+      " OR public = 1;";
 
-			data = JSON.stringify(result);
-			console.log("Data: " + data);
+    console.log(getSBdataSQL);
 
-			res.render('dashboard.ejs', {
-				email: req.session.email,
-				firstName: req.session.firstName,
-				lastName: req.session.lastName,
-				userid: req.session.userid,
-				data: result
-			});	
-		});
-	})
+    conn.query(getSBdataSQL, function(err, result) {
+      if (err) throw err;
 
-	// INDIVIDUAL SOUNDBOARD PAGE
-	app.get('/app/:userid/soundboard/:sbid', function (req, res) {
-		console.log(req.params);
-		req.session.currsbid = req.params.sbid;
+      data = JSON.stringify(result);
+      console.log("Data: " + data);
 
-		var getSoundsSQL = "SELECT * FROM sounds " + 
-		"WHERE sbid = " + req.params.sbid
+      res.render("dashboard.ejs", {
+        email: req.session.email,
+        firstName: req.session.firstName,
+        lastName: req.session.lastName,
+        userid: req.session.userid,
+        data: result
+      });
+    });
+  });
 
-		conn.query(getSoundsSQL, function (err, result) {
-			if (err) throw err;
+  // INDIVIDUAL SOUNDBOARD PAGE
+  app.get("/app/:userid/soundboard/:sbid", function(req, res) {
+    console.log(req.params);
+    req.session.currsbid = req.params.sbid;
 
-			res.render('soundboard.ejs', {
-				email: req.session.email,
-				firstName: req.session.firstName,
-				lastName: req.session.lastName,
-				userid: req.session.userid,
-				data: result
-			});
-		});
-	})
+    var getSoundsSQL =
+      "SELECT * FROM sounds " + "WHERE sbid = " + req.params.sbid;
 
-	app.post('/app/upload', function(req, res){
-		upload(req, res, function(err) {
-			if (err)
-				return res.end("Error uploading file.");
+    conn.query(getSoundsSQL, function(err, result) {
+      if (err) throw err;
 
-			console.log(req.files[0].filename);
-			console.log(req.files[1].filename);
+      res.render("soundboard.ejs", {
+        email: req.session.email,
+        firstName: req.session.firstName,
+        lastName: req.session.lastName,
+        userid: req.session.userid,
+        data: result
+      });
+    });
+  });
 
-			var addNewSoundSQL = 
-			"INSERT INTO sounds (sbid, name, image, sound) " +
-			"VALUES (" + req.session.currsbid + ", \'" 
-				+ req.body.soundName + "\', \'/uploads/" 
-				+ req.files[0].filename + "\', \'/uploads/" 
-				+ req.files[1].filename + "\')";
+  app.post("/app/upload", function(req, res) {
+    upload(req, res, function(err) {
+      if (err) return res.end("Error uploading file.");
 
-			console.log(addNewSoundSQL);
+      console.log(req.files[0].filename);
+      console.log(req.files[1].filename);
 
-			conn.query(addNewSoundSQL, function (err, result) {
-				if (err) throw err;
+      var addNewSoundSQL =
+        "INSERT INTO sounds (sbid, name, image, sound) " +
+        "VALUES (" +
+        req.session.currsbid +
+        ", '" +
+        req.body.soundName +
+        "', '/uploads/" +
+        req.files[0].filename +
+        "', '/uploads/" +
+        req.files[1].filename +
+        "')";
 
-				res.redirect('/app/' + req.session.userid + '/soundboard/' + req.session.currsbid);
-			});
-		});
-	})
+      console.log(addNewSoundSQL);
 
-	// LOG OUT OF SESSION
-	app.get('/logout', function(req, res) {
-		req.session.destroy();
-		console.log("Session logout");
-		res.redirect('/');
-	})
+      conn.query(addNewSoundSQL, function(err, result) {
+        if (err) throw err;
 
-}
+        res.redirect(
+          "/app/" + req.session.userid + "/soundboard/" + req.session.currsbid
+        );
+      });
+    });
+  });
+
+  // LOG OUT OF SESSION
+  app.get("/logout", function(req, res) {
+    req.session.destroy();
+    console.log("Session logout");
+    res.redirect("/");
+  });
+};
--- server.js
+++ server.js
@@ -1,66 +1,68 @@
-const express	= require('express')
-const app		= express()
-const port		= process.env.PORT || 8081
-const mysql		= require('mysql')
-const multer	= require('multer')
-const mime 		= require('mime-types')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8081;
+const mysql = require("mysql");
+const multer = require("multer");
+const mime = require("mime-types");
 
-const morgan		= require('morgan')
-const bodyParser	= require('body-parser')
-const session		= require('express-session')
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 
-const path		= require('path')
+const path = require("path");
 
 // MySQL db config
-const conn	= mysql.createConnection({
-	host		: "ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-	port 		: "3306",
-	user		: "yfa1b5xr3062ctem",
-	password	: "enpavzdbydherk36",
-	database 	: "apz88ekivr2vs5ov"
+const conn = mysql.createConnection({
+  host: "ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: "3306",
+  user: "yfa1b5xr3062ctem",
+  password: "enpavzdbydherk36",
+  database: "apz88ekivr2vs5ov"
 });
 
 // Connect to MySQL database
 conn.connect(function(err) {
-	if (err) throw err;
-	console.log("Connected to MySQL database.");
-})
-
-// SETUP EXPRESS application
-app.use(morgan('dev'))	// log every request to the console
-app.use(bodyParser.urlencoded( { extended: true })) // get info from HTML forms
-app.use(bodyParser.json())
-
-// sets up ejs as view engine
-app.set('view engine', 'ejs')
-
-// path to all public file (CSS, JS, imgs, etc.)
-app.use(express.static(__dirname + '/public'))
-
-// setup session
-app.use(session({
-	secret: 'ilovebourbon',
-	resave: true,
-	saveUninitialized: true
-}))
-
-// FILE UPLOAD
-const storage	= multer.diskStorage({
-	destination: function (req, file, callback) {
-		callback(null, __dirname + '/public/uploads');
-	},
-	filename: function (req, file, callback) {
-		// file.fieldname + '-' + 
-		var newFileName = Date.now() + '.' + mime.extension(file.mimetype);
-		callback(null, newFileName);
-	}
+  if (err) throw err;
+  console.log("Connected to MySQL database.");
 });
 
-var upload = multer({ storage: storage }).array('userFile', 2);
+// SETUP EXPRESS application
+app.use(morgan("dev")); // log every request to the console
+app.use(bodyParser.urlencoded({ extended: true })); // get info from HTML forms
+app.use(bodyParser.json());
+
+// sets up ejs as view engine
+app.set("view engine", "ejs");
+
+// path to all public file (CSS, JS, imgs, etc.)
+app.use(express.static(__dirname + "/public"));
+
+// setup session
+app.use(
+  session({
+    secret: "ilovebourbon",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// FILE UPLOAD
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, __dirname + "/public/uploads");
+  },
+  filename: function(req, file, callback) {
+    // file.fieldname + '-' +
+    var newFileName = Date.now() + "." + mime.extension(file.mimetype);
+    callback(null, newFileName);
+  }
+});
+
+var upload = multer({ storage: storage }).array("userFile", 2);
 
 // APP ROUTES
-const routes = require('./app/routes/routes')(app, express, conn, upload);
+const routes = require("./app/routes/routes")(app, express, conn, upload);
 
-// CONNECT 
-app.listen(port)
-console.log('The magic happens on port ' + port)
+// CONNECT
+app.listen(port);
+console.log("The magic happens on port " + port);
