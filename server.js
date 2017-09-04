0a5ce636979eddb335e2dd4f44621caf7659fa78
Miguel Vargas <miguelvargas.121@gmail.com>
2017-09-04 00:29:21 -0700

    UPDATE: few edits

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 const mysql = require("mysql");
 const multer = require("multer");
 const mime = require("mime-types");
